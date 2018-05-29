c811369127c4670b612beb39947abdbfc71bf532
Reggiecril <reggiecril0618@gmail.com>
2018-05-29 18:49:09 +0800

    twoSum

---
+++ src/twoSum/demo.java
@@ -0,0 +1,38 @@
+package twoSum;
+
+public class demo {
+    public int[] twoSum(int[] nums, int target) {
+        int []number=new int[2];
+        for(int i=0;i<nums.length;i++){
+        	boolean isTarget=false;
+            for(int j=0;j<nums.length;j++){
+            	if(j!=i) {
+	                if(nums[i]+nums[j]==target){
+	                	
+	                	isTarget=true;
+	                    number[0]=i;
+	                    number[1]=j;
+	                }
+            	}
+            }
+            
+            if(isTarget)
+            break;
+            
+        } return number;
+    }
+public static void main(String []args) {
+	demo c=new demo();
+	int[] nums = {2, 7, 11, 15};
+	int[] nums1 = {3,2,4};
+	
+	int target = 9;
+	int []number=c.twoSum(nums, target);
+	int []number1=c.twoSum(nums1, 6);
+	for(int i:number)
+		System.out.println(i);
+	System.out.println("+++++++++++++++++++++++++++++++");
+	for(int i:number1)
+		System.out.println(i);
+}
+}
