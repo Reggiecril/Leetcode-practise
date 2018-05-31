110a9a3fff697a386f5f1f9255721dc3e7e633b4
Reggiecril <reggiecril0618@gmail.com>
2018-05-31 10:21:23 +0800

    update reveserInteger

--- src/reverseInteger/Demo.java
+++ src/reverseInteger/Demo.java
@@ -5,23 +5,18 @@ public class Demo {
 	
 	 public int reverse(int x) {
 		 try {
-		        String[] num=String.valueOf(x).split("");
-		        int count=num.length;
-		        String []afterNum=new String[num.length];
-		        for(int i=0;i<num.length;i++) {
-		        	if(num[0].equals("-")) {
-		        		if(i+1==num.length)
-		        			break;
-		        		afterNum[0]="-";
-		        		afterNum[i+1]=num[count-1];
-		        	}else
-		        		afterNum[i]=num[count-1]; 
-		        	count--;
-		        }
-		        String last = "";
-		        for(String line:afterNum)
-		        	last+=line;
-		        int lastNum=Integer.valueOf(last);
+			 	double n=Math.signum(x);
+			 	String negitive="";
+			 	
+			 	
+		        String num=String.valueOf(Math.abs(x));
+		        StringBuilder sb=new StringBuilder();
+		        sb.append(num);
+		        int lastNum=0;
+		        if(n<0)
+		        	lastNum=-Integer.valueOf(sb.reverse().toString());
+		        else
+		        	lastNum=Integer.valueOf(sb.reverse().toString());
 		        return lastNum;
 	        }catch(RuntimeException re) {
 	        	return 0;
@@ -34,7 +29,7 @@ public class Demo {
 	public static void main(String[] args) {
 		Demo demo=new Demo();
 	
-		System.out.println(demo.reverse(964351));
+		System.out.println(demo.reverse(-964351));
 
 	}
 
