18c29e2d0d5d651255e514f791046b9bfe789771
Reggiecril <reggiecril0618@gmail.com>
2018-06-02 18:11:15 +0800

    description for valid Parentheses

--- src/validParentheses/Demo.java
+++ src/validParentheses/Demo.java
@@ -1,7 +1,18 @@
 package validParentheses;
 
 public class Demo {
-
+	/**
+	 * 
+	 * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+	 *An input string is valid if:
+	 *Open brackets must be closed by the same type of brackets.
+	 *Open brackets must be closed in the correct order.
+	 *
+	 *Input: "{[]}"
+	 *Output: true
+	 * @param s
+	 * @return
+	 */
 	 public boolean isValid(String s) {
 	       String a = s;
 			for (int i = 0; i < s.length(); i++) {
