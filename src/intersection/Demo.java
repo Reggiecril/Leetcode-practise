b00cd0339c8825ef1fb7a5b0228e2e0803bfe760
Reggiecril <reggiecril0618@gmail.com>
2018-07-16 17:20:14 +0800

    update

--- src/intersection/Demo.java
+++ src/intersection/Demo.java
@@ -1,6 +1,7 @@
 package intersection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
--- src/removeNthFromEnd/Demo.java
+++ src/removeNthFromEnd/Demo.java
@@ -7,6 +7,15 @@ package removeNthFromEnd;
  */
 public class Demo {
 	/**
+	 * Given a linked list, remove the n-th node from the end of list and return its
+	 * head.
+	 * 
+	 * Example:
+	 * 
+	 * Given linked list: 1->2->3->4->5, and n = 2.
+	 * 
+	 * After removing the second node from the end, the linked list becomes
+	 * 1->2->3->5.
 	 * 
 	 * @param head
 	 * @param n
