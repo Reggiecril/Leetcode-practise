797eb5532629d93b873577212fe98daf70832f72
Reggiecril <reggiecril0618@gmail.com>
2018-08-17 13:56:24 +0800

    combination Sum 3 update

--- src/combinationSum3/Demo.java
+++ src/combinationSum3/Demo.java
@@ -1,4 +1,26 @@
 package combinationSum3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list=new ArrayList<>();
+        combination(list,new ArrayList<>(),1,k,n);
+        return list;
+
+    }
+    private void combination(List<List<Integer>> outList,List<Integer> inList,int start,int k,int n){
+
+        if(inList.size()==k&&n==0){
+            outList.add(new ArrayList<>(inList));
+            return;
+        }else {
+            for (int i = start; i < 10; i++) {
+                inList.add(i);
+                combination(outList, inList, i + 1, k, n - i);
+                inList.remove(inList.size() - 1);
+            }
+        }
+    }
 }
