d693fb1f6ff2314f2749338913ddb23d5832d227
Reggiecril <reggiecril0618@gmail.com>
2018-10-10 21:21:55 +0100

    Decode Ways

--- src/decodeWays/Demo.java
+++ src/decodeWays/Demo.java
@@ -1,4 +1,23 @@
 package decodeWays;
 
 public class Demo {
+    public int numDecodings(String s) {
+        //get s length
+        int length=s.length();
+        int n=length+1;
+        //initalize a array
+        int[] newInt=new int[n];
+        //value last integer
+        newInt[n-1]=1;
+        //0:1
+        newInt[n-2]=s.charAt(n-2)=='0'?0:1;
+        //[2,3,4,2,3,4]
+        //[4,2,2,2,2,1,1]
+        for(int i=n-3;i>=0;i--){
+            if(s.charAt(i)=='0')continue;
+            else
+                newInt[i]=(Integer.parseInt(s.substring(i,i+2)))<=26?newInt[i+1]+newInt[i+2]:newInt[i+1];
+        }
+        return newInt[0];
+    }
 }
